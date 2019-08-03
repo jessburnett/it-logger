67aa40ad9c794a0c8bc3723f929a32e3fc3b8ef2
jessburnett <jess.a.burnett@gmail.com>
2019-08-03 08:26:08 -0400

    LogItem component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import LogItem from './LogItem'
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -14,7 +15,7 @@ const Logs = () => {
     const res = await fetch('/logs');
     const data = await res.json();
     
-    setLogs(data);
+    setLogs(data);     //eslint-disable-next-line
     setLoading(false);
 
     if(loading){
@@ -23,10 +24,10 @@ const Logs = () => {
   }
 
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header"><h4 className="center">System Logs</h4></li>
       {!loading && logs.length === 0 ? (<p className="center">No Logs to show...</p>) : (
-        logs.map(log => <li>{log.message}</li>)
+        logs.map(log => <LogItem log={log} key={log.id}/>)
       )}
     </ul>
   )
